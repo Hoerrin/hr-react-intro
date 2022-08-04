1d06bb3992e60bda9553b12c43eb9e6fee070fee
Hoerrin <patrykd0604@gmail.com>
2022-08-04 13:03:11 +0200

    Finished part 1

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const Paragraph = () => {
+    return React.createElement(
+        'p',
+        {
+            id: 'firs-paragraph',
+            className: 'paragraph'
+        },
+        'Lorem Ipsum'
+    )
+}
+
+const appContainer = document.querySelector('#app');
+
+ReactDOM.render(React.createElement(Paragraph), appContainer)
